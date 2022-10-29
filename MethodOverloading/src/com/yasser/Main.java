a11bfc1769f5cf32d270255a3dfe3a32a4a73b34
Yasser Bal <yasserbal@icloud.com>
2022-10-29 21:29:54 +0200

    Finished Seconds and Minutes challenge Also covering cases where minutes and seconds can be over 60

--- MethodOverloading/src/com/yasser/Main.java
+++ MethodOverloading/src/com/yasser/Main.java
@@ -6,18 +6,46 @@ Method Overloading allows different methods to have the same name,
   or a mixture of both.
 
   Method overloading is also known as Compile-time Polymorphism, Static Polymorphism, or Early binding in Java
- */
+
+  //THIS IS A FEATURE
+  //A method can have the same name as long as params differ
+  //The logic within these methods can differ
+  //helps to avoid:
+  //sumOfTwoNumbers(int a, int b) - sumOfThreeNumbers(int a, int b, int c) - sumOfFourNumbers(int a,int b, int c, int d)
+   //^ All of above method names could be declared with the same name
+   */
 public class Main {
     final static double ONE_INCH_IN_CM = 2.54;
     final static double ONE_FOOT_IN_INCHES = 12;
+
+    final static int SECONDS_IN_ONE_MINUTE = 60;
+    final static int SECONDS_IN_ONE_HOUR = 3600;
+    final static int MINUTES_IN_ONE_HOUR = 60;
     public static void main(String[] args) {
         System.out.println("Hello Static Polymorphism!");
+        System.out.println(getDurationString(5,5));
+        System.out.println(getDurationString(500,500));
+        System.out.println(getDurationString(60,60));
+        System.out.println(getDurationString(5000));
 
-//        System.out.println(calcFeetAndInchesToCentimeters(1,0));
-//        System.out.println(calcFeetAndInchesToCentimeters(1));
-//        System.out.println(calcFeetAndInchesToCentimeters(0,1));
-//        System.out.println(calcFeetAndInchesToCentimeters(5));
-//        System.out.println(calcFeetAndInchesToCentimeters(5,5));
+    }
+    //SECONDS AND MINUTES CHALLENGE
+    private static String getDurationString(int minutes,int seconds){
+        if((!isPositiveNumber(minutes))||(!isPositiveNumber(seconds))){
+            return "Invalid value";
+        }
+        int totalSeconds = (minutes*SECONDS_IN_ONE_MINUTE)+seconds;
+        int totalHours = (totalSeconds/SECONDS_IN_ONE_HOUR>=1)? ((int) Math.floor(totalSeconds/SECONDS_IN_ONE_HOUR)) : 0;
+        int remainingSeconds = totalSeconds-(totalHours*SECONDS_IN_ONE_HOUR);
+        int remainingMinutes = (remainingSeconds/SECONDS_IN_ONE_MINUTE>=1)? ((int) Math.floor(remainingSeconds/SECONDS_IN_ONE_MINUTE)):0;
+        remainingSeconds-=remainingMinutes*SECONDS_IN_ONE_MINUTE;
+        return totalHours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+    }
+    private static String getDurationString(int seconds){
+        return getDurationString(0,seconds);
+    }
+    private static boolean isPositiveNumber(int x){
+        return (x>=0);
     }
     private static double calcFeetAndInchesToCentimeters(double feet,double inches){
 
