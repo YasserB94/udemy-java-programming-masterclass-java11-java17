fdb6146647f94ea357ceaaaedbf8a3eb3ff9f39e
Yasser Bal <yasserbal@icloud.com>
2022-10-29 12:01:12 +0200

    Finished Double Challenge

---
+++ doublesChallenge/src/learning/academy/Main.java
@@ -0,0 +1,16 @@
+package learning.academy;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello floating point numbers!");
+        /*
+        * CHALLENGE
+        * Create a variable containing a number of pounds that will be converted to kilograms
+        * Calculate the result and store it into another variable
+        * */
+        double conversionValue = 0.453_592_37d;
+        double pounds = 200d;
+        double kilograms = pounds*conversionValue;
+        System.out.println(pounds + " pounds in kilograms equals: " + kilograms);
+        
+    }
+}
