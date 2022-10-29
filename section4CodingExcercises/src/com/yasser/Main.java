fc19277c52a28639b2e34352df965247596f7ef0
Yasser Bal <yasserbal@icloud.com>
2022-10-29 17:49:49 +0200

    Completed SpeedConverter Challenge

--- section4CodingExcercises/src/com/yasser/Main.java
+++ section4CodingExcercises/src/com/yasser/Main.java
@@ -2,5 +2,17 @@ package com.yasser;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Excercises!");
+        //testSpeedConverter();
+    }
+    private static void testSpeedConverter(){
+        System.out.println();
+        System.out.println("SpeedConverter Test Start");
+        double[] speedConverterTestCases = {1.5,10.25,-5.6,25.42,75.114};
+        for (double input:speedConverterTestCases
+        ) {
+            SpeedConverter.printConversion(input);
+        }
+        System.out.println("SpeedConverter Test End");
+        System.out.println();
     }
 }
