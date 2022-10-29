5d627f2c7821e448e270ee8a9dec9e87f41d919f
Yasser Bal <yasserbal@icloud.com>
2022-10-29 09:46:56 +0200

    Added Variables

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        String greeting = "Hello Java!";
+        System.out.println(greeting);
+
+        int sumOfNumbers = (10*10)+(80-9);
+        System.out.println(sumOfNumbers);
     }
 }
 
