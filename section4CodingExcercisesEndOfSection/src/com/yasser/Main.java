083899a8831275e4acb0c487f5a73baaca42c62c
Yasser Bal <yasserbal@icloud.com>
2022-10-29 22:50:16 +0200

    Finished MinutesToYearsAndDays Excercise

--- section4CodingExcercisesEndOfSection/src/com/yasser/Main.java
+++ section4CodingExcercisesEndOfSection/src/com/yasser/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Area Calculator!");
-
+        MinutesToYearsDaysCalculator.printYearsAndDays(525_600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1_051_200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561_600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(689_450);
+        MinutesToYearsDaysCalculator.printYearsAndDays(9_000);
     }
 }
