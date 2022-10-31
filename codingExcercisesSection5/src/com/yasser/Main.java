a087d3bf7d60119ae9ede1725a0d6b2ce8bdac95
Yasser Bal <yasserbal@icloud.com>
2022-10-31 12:08:03 +0100

    Finished EvenDigitSum exercise

--- codingExcercisesSection5/src/com/yasser/Main.java
+++ codingExcercisesSection5/src/com/yasser/Main.java
@@ -2,7 +2,7 @@ package com.yasser;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Section 5! \n");
-        System.out.println(FirstAndLastDigit.sumFirstAndLastDigit(10));
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));
     }
     private static void testSumOdd(){
         class TestCase{
