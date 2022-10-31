12b9ab40310db8af90950d32144e370f0dee058d
Yasser Bal <yasserbal@icloud.com>
2022-10-31 22:23:35 +0100

    Completed Person Exercise

---
+++ exercisesSection6/src/com/yasser/Person.java
@@ -0,0 +1,46 @@
+package com.yasser;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public boolean isTeen(){
+        return (this.age>12&&this.age<20);
+    }
+    public String getFullName(){
+        if(this.firstName.isEmpty()&&this.lastName.isEmpty()){
+            return "";
+        }
+        if(this.firstName.isEmpty()){
+            return this.lastName;
+        }
+        if(this.lastName.isEmpty()){
+            return this.firstName;
+        }
+        return this.firstName + " " + this.lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = ((age < 0) || (age > 100)) ? 0 : age;
+    }
+}
