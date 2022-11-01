d5f67d966da399e626f1bbf0eaba2c576a8b2dea
Yasser Bal <yasserbal@icloud.com>
2022-11-01 15:30:40 +0100

    Completed SortedArray exercise

---
+++ exercisesSection8/src/com/yasser/Main.java
@@ -0,0 +1,8 @@
+package com.yasser;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Section 8!!");
+        SortedArray sorter = new SortedArray();
+        sorter.printArray(sorter.sortIntegers(sorter.getIntegers(5)));
+    }
+}
