c0439b296f09dbd7e4a6e768c9b43bcdb6628880
Yasser Bal <yasserbal@icloud.com>
2022-11-01 14:47:03 +0100

    removed billsburgers

--- billsBurgers/src/com/yasser/Main.java
+++ billsBurgers/src/com/yasser/Main.java
@@ -1,6 +1,10 @@
 package com.yasser;
 public class Main {
+    int[] tt = {106,26,81,5,15};
     public static void main(String[] args) {
         System.out.println("Hello Bill's Burgers!");
+    }
+    public void sort(){
+        tt.so
     }
 }
