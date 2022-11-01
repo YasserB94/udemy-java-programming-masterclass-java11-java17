385801ee7e55efa7582b061b1cb3a2ec89b2d9b4
Yasser Bal <yasserbal@icloud.com>
2022-11-01 18:38:51 +0100

    Completeed ReverseArray Exercise

---
+++ exercisesSection8/src/com/yasser/ReverseArray.java
@@ -0,0 +1,19 @@
+package com.yasser;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] l = {4,29,4,3,2,1,9};
+        reverse(l);
+    }
+    private static void reverse(int[] array){
+        System.out.println("Array = " + Arrays.toString(array));
+        int[] reversed = new int[array.length];
+
+        for (int i = array.length-1,j = 0; i >=0 ; i--,j++) {
+            reversed[j] = array[i];
+        }
+        System.out.println("Reversed array = " + Arrays.toString(reversed));
+    }
+}
