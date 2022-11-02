0f686a0fe2d0e731f98166e3e6a7702bf3c67b5f
YasserB94 <yasserbal@icloud.com>
2022-11-02 16:08:36 +0100

    Completed MobilePhone Exercise

--- exercisesSection8/src/com/yasser/Main.java
+++ exercisesSection8/src/com/yasser/Main.java
@@ -1,9 +1,18 @@
 package com.yasser;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Section 8!!");
-        System.out.println(
-
-        );
+        MobilePhone phone = new MobilePhone("mp");
+        for (int i = 0; i < 10; i++) {
+            phone.addNewContact(Contact.createContact("Contact "+i, "0"+i));
+        }
+        Contact hi = Contact.createContact("Hello","World");
+        Contact helloWorld = Contact.createContact("Hello","World");
+        System.out.println(phone.addNewContact(hi));
+        System.out.println(phone.addNewContact(hi));
+        System.out.println(phone.addNewContact(helloWorld));
+        phone.printContacts();
     }
 }
